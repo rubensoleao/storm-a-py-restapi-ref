1bb744781dfb1f517516453a62d7fac7f10ab62d
Rubens Leao <rubensoleao@gmail.com>
2021-07-18 20:31:55 -0300

    Added MsgScheduleClient

--- mglu/db/client.py
+++ mglu/db/client.py
@@ -1,9 +1,11 @@
 import logging
+from contextlib import contextmanager
 
 import mglu.settings as settings
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, delete
+from sqlalchemy.orm import Session
 
-from .models import Base
+from .models import Base, MsgSchedules
 
 
 class SQLClient:
@@ -22,5 +24,57 @@ class SQLClient:
     def create_tables(self):
         Base.metadata.create_all(self.engine)
 
-    def get_connection(self):
+    @property
+    def connection(self):
         return self.engine.connect()
+
+
+# Session = sessionmaker(bind=client.engine)
+class MsgScheduleClient(SQLClient):
+    def __init__(self) -> None:
+
+        super().__init__()
+
+    @contextmanager
+    def get_session(self):
+        session = Session(self.engine)
+        try:
+            yield session
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    def create(self, args):
+        with self.get_session() as session:
+            new = MsgSchedules(**args)
+            session.add(new)
+            session.commit()
+            return {"id": new.id}
+
+    def delete(self, id):
+        stmt = delete(MsgSchedules).where(MsgSchedules.id == id)
+
+        with self.get_session() as session:
+            session.execute(stmt)
+
+            return {"id": id}
+
+    def get(self, id):
+        with self.get_session() as session:
+            return session.query(MsgSchedules).filter(MsgSchedules.id == id).first()
+
+    def paginated(self, page, per_page):
+        with self.get_session() as session:
+            return (
+                session.query(MsgSchedules)
+                .offset(page * per_page)
+                .limit(per_page)
+                .all()
+            )
+
+    def filter(self, args):
+        with self.get_session() as session:
+            return session.query(MsgSchedules).filter_by(**args).all()
