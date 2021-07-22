9b6a45ed4538780fd651083cd51c882fed5b559e
Rubens Leao <rubensoleao@gmail.com>
2021-07-22 00:43:26 -0300

    Seperated each flask route by method

--- app.py
+++ app.py
@@ -13,42 +13,41 @@ from .mglu.services.functions import delete_schedule, get_schedule, post_schedul
 app = Flask(__name__)
 
 
-@app.route("/msg-scheduler", methods=["POST", "GET", "DELETE"])
-def hello_world():
-    if flask_request.method == "POST":
-        request_data = flask_request.get_json()
+@app.route("/msg-scheduler", methods=["POST"])
+def msg_scheduler_POST():
+    request_data = flask_request.get_json()
 
-        if not flask_request:
-            raise MissingPostData
+    if not flask_request:
+        raise MissingPostData
 
-        processed_response = post_schedule(client, request_data)
+    processed_response = post_schedule(client, request_data)
 
-        return app.response_class(
-            response=json.dumps(processed_response),
-            status=HTTPStatus.CREATED,
-        )
-    elif flask_request.method == "GET":
-        request_id = {"id": flask_request.args.get("id")}
-        processed_response = get_schedule(client, request_id)
-        return app.response_class(
-            status=HTTPStatus.OK,
-            response=json.dumps(processed_response),
-            mimetype="application/json",
-        )
-    elif flask_request.method == "DELETE":
-        request_id = {"id": flask_request.args.get("id")}
-        processed_response = delete_schedule(client, request_id)
-        return app.response_class(
-            status=HTTPStatus.OK,
-            response=json.dumps(processed_response),
-            mimetype="application/json",
-        )
-    else:
-        return app.response_class(
-            status=HTTPStatus.BAD_REQUEST,
-            response="Unsupported method",
-            mimetype="application/json",
-        )
+    return app.response_class(
+        response=json.dumps(processed_response),
+        status=HTTPStatus.CREATED,
+    )
+
+
+@app.route("/msg-scheduler", methods=["GET"])
+def msg_scheduler_GET():
+    request_id = {"id": flask_request.args.get("id")}
+    processed_response = get_schedule(client, request_id)
+    return app.response_class(
+        status=HTTPStatus.OK,
+        response=json.dumps(processed_response),
+        mimetype="application/json",
+    )
+
+
+@app.route("/msg-scheduler", methods=["DELETE"])
+def msg_scheduler_DELETE():
+    request_id = {"id": flask_request.args.get("id")}
+    processed_response = delete_schedule(client, request_id)
+    return app.response_class(
+        status=HTTPStatus.OK,
+        response=json.dumps(processed_response),
+        mimetype="application/json",
+    )
 
 
 @app.errorhandler(NoResultFound)
@@ -79,5 +78,14 @@ def handle_validation_error(e):
     return app.response_class(
         status=HTTPStatus.BAD_REQUEST,
         response=json.dumps({"message": message}),
+        mimetype="application/json",
+    )
+
+
+@app.errorhandler(500)
+def internal_error():
+    return app.response_class(
+        status=HTTPStatus.INTERNAL_SERVER_ERROR,
+        response="Internal server error",
         mimetype="application/json",
     )
